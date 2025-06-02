fb11dc2542d77b25cba043bc615ce537b52ef68c
Louis Denys <louis.denys@u-bordeaux.fr>
2025-06-02 11:11:35 +0200

    creation de matrice depuis un dossier de donnees

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+import os
+
+DATADIR = "data"
+
 # 1. Create a tab from a game
 data = []
 
-with open("sample.pgn", "r") as file:
+print("#################################################")
+print("LECTURE DES FICHIERS")
+for file in os.listdir(DATADIR):
+    with open(DATADIR + "/" + file, "r", errors='ignore') as file:
+        print(file)
 
-    # Read only the gameplay part
-    
-    reader = file.read().split("\n\n")
-    for i in range(1, len(reader), 2):
-        data.append(reader[i])
+        # Read only the gameplay part
+        
+        reader = file.read().split("\n\n")
+        for i in range(1, len(reader), 2):
+            data.append(reader[i])
 
-    # On remplace les retour à la ligne par des espaces
-    # Split pour récupérer les coups un à un
-    for i in range(len(data)):
-        data[i] = data[i].replace("\n", " ")
-        data[i] = data[i].split(" ")
+
+# On remplace les retour à la ligne par des espaces
+# Split pour récupérer les coups un à un
+print("#################################################")
+print("EXTRACTION DES COUPS:")
+
+for i in range(len(data)):
+    print(f'Extraction des coups: {round(i/len(data)*100)}%')
+    data[i] = data[i].replace("\n", " ")
+    data[i] = data[i].split(" ")
 
 final = [ [] for i in range(len(data))]
 
+
+print("#################################################")
+print("FORMATAGE DES COUPS:")
+
 # Formatage d'un coup en retirant le numéro de manche
 for i in range(len(data)):
+    print(f'Formatage des coups: {round(i/len(data)*100)}%')
     for move in data[i]:
         move = move.split(".")
         try:
