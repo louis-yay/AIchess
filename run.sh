26eb5a59960e932672988ef294a010ae7addda50
Louis Denys <louisdenys0@gmail.com>
2025-07-16 15:16:13 +0200

    setup for server training

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define names
+VENV_DIR=".venv"
+REQUIREMENTS_FILE="requirement.txt"
+MAIN_FILE="constructor.py"
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment in $VENV_DIR..."
+    python3 -m venv "$VENV_DIR"
+else
+    echo "Virtual environment already exists."
+fi
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source "$VENV_DIR/bin/activate"
+
+# Install dependencies
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    echo "Installing dependencies from $REQUIREMENTS_FILE..."
+    pip install --upgrade pip
+    pip install -r "$REQUIREMENTS_FILE"
+else
+    echo "[ERROR] No requirements.txt found. Skipping dependency installation."
+fi
+
+# Download kaggle dataset
+if [ -f chess_games.csv ]; then
+    echo "dataset allready downloaded"
+else
+    kaggle datasets download arevel/chess-games
+    unzip -qq chess-games.zip
+    echo "dataset downloaded"
+    rm chess-games.zip
+fi
+
+
+# Run the main Python script
+if [ -f "$MAIN_FILE" ]; then
+    echo "Running $MAIN_FILE..."
+    python "$MAIN_FILE"
+else
+    echo "[ERROR] $MAIN_FILE not found. Cannot run script."
+fi
